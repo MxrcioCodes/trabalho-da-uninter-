344f96c2b9dfc39341fbe9110644e5e7fbe11e00
MxrcioCodes <mgabriel.gss@gmail.com>
2025-06-23 18:21:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let botao = document.getElementById ('botao')
+let mensagem = document.getElementById ('mensagem')
+
+
+botao.addEventListener ('click', function(){
+   mensagem.textContent = '"Grandes conquistas começam com pequenas decisões diárias. O esforço de hoje constrói os resultados de amanhã."';
+});
+
